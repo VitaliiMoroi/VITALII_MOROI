f657d6e2dd28a9417fd2ebdf5c69f89a19e4b2a7
VitaliiMoroi <rey4fire@gmail.com>
2024-03-29 14:17:20 +0200

    This is my second homework. Added branch homework_nr_2. Added package HomeWork2.java. Added homework code in HomeWork2.java.

---
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -0,0 +1,41 @@
+package homework_nr_2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+       long CurrentWorldPopulation = 7811080815L;
+
+       int CarsProducedThisYear = 55235358;
+
+       char FirstLetterInAlphabet = 'A';
+
+       boolean JavaIsAmazing = true;
+
+       float Pi = 3.14159F;
+
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(JavaIsAmazing);
+        System.out.println(Pi);
+
+        double Price = 25.50;
+        double Tax = 0.50;
+        int Quantity = 15;
+        double Sum = 0;
+
+        System.out.println(Price);
+        System.out.println(Tax);
+        System.out.println(Quantity);
+        System.out.println(Sum);
+
+        double Total = Price + Tax * Quantity + Sum;
+
+        System.out.println(Total);
+
+
+
+    }
+}
